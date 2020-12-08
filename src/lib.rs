ef134f7ee51d9489667687ba4f3fa0b33f047dc1
Daniel Kerbel <nmdanny@gmail.com>
2020-12-08 21:34:31 +0200

    Get rid of ex1 stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(type_alias_impl_trait)]
 #![allow(unused_imports)]
 #![allow(dead_code)]
 #[macro_use]
@@ -7,15 +6,5 @@ pub extern crate tracing;
 #[macro_use]
 pub extern crate derivative;
 
-mod adversary;
-mod client;
-mod server;
-mod types;
 pub mod consensus;
 
-pub use adversary::start_adversary;
-pub use server::start_server;
-pub use types::{
-    ChatClient, ChatUpdated, ConnectRequest, LogRequest, LogResponse, PacketMetadata, Settings,
-    WriteRequest,
-};
