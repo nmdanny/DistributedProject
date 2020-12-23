6d11337a8f5f8a35b11fba003a99953203ef7a63
Daniel Kerbel <nmdanny@gmail.com>
2020-12-23 16:41:36 +0200

    Impl share encoding/decoding

--- src/anonymity/secret_sharing.rs
+++ src/anonymity/secret_sharing.rs
@@ -5,15 +5,29 @@ use std::collections::BTreeSet;
 use num_traits::*;
 use gridiron::fp_256::Fp256;
 use rand::seq::SliceRandom;
-
+use serde::{Serialize, Deserialize};
+use serde::de::DeserializeOwned;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hasher, Hash};
+use std::convert::TryFrom;
 
 // A finite field with 256 bits
-type FP = Fp256;
+pub type FP = Fp256;
 
 /// A secret share
 pub struct Share {
     x: FP,
     p_x: FP
+}
+
+impl Share {
+    /// Creates a share for 'x' using the 
+    pub fn new(x: u32) -> Share {
+        Share {
+            x: x.into(),
+            p_x: 0u32.into()
+        }
+    }
 }
 
 /// Represents a polynomial 'f(x) = c0 + c1*x + c2*x^2 + ....' 
@@ -44,7 +58,7 @@ impl Polynomial {
 }
 
 /// Splits the secret into `n` shares, needing `k` shares to re-construct
-fn create_share(secret: FP, k: u32, n: u32) -> Vec<Share> {
+pub fn create_share(secret: FP, k: u32, n: u32) -> Vec<Share> {
     assert!(k <= n, "number of shares can't be bigger than n");
     assert!(k >= 1, "number of shares must be at least 1");
 
@@ -60,7 +74,7 @@ fn create_share(secret: FP, k: u32, n: u32) -> Vec<Share> {
 }
 
 /// Reconstructs a secret from a given list of secret shares, assuming secret was split to 'k' different shares
-fn reconstruct_secret(shares: &[Share], k: u32) -> FP {
+pub fn reconstruct_secret(shares: &[Share], k: u32) -> FP {
     assert!(shares.len() >= k as usize, "need at least k different points");
     assert_eq!(shares.iter().map(|s| s.x).collect::<BTreeSet<_>>().len(), shares.len(),
                "all shares must have unique 'x' values");
@@ -77,6 +91,68 @@ fn reconstruct_secret(shares: &[Share], k: u32) -> FP {
         result += (*p_xj) * el_j;
     }
     return result;
+}
+
+#[derive(Serialize, Deserialize)]
+struct ValueAndHash<V> {
+    value: V,
+    hash: u64
+}
+
+pub fn encode_secret<S: Serialize + Hash>(data: S) -> Result<FP, anyhow::Error> {
+    let mut value_and_hash = ValueAndHash {
+        value: data,
+        hash: 0
+    };
+    let mut hasher = DefaultHasher::new();
+    value_and_hash.value.hash(&mut hasher);
+    value_and_hash.hash = hasher.finish();
+    let bytes = bincode::serialize(&value_and_hash)?;
+    if bytes.len() > 31 {
+        return Err(anyhow::anyhow!("Need more than 31 bytes to safely store value, does not fit into secret"));
+    }
+
+    let mut secret_bytes = [0u8; 32];
+    &secret_bytes[ .. bytes.len()].copy_from_slice(&bytes);
+
+    let fp = FP::from(secret_bytes);
+    return Ok(fp)
+
+}
+
+pub fn encode_zero_secret() -> FP {
+    0u32.into()
+}
+
+/// Given a secret(or what we suspect is a secret), tries to deserialize its contents
+/// and return the resulting value. If no value is contained(e.g, no client sent a value), None is returned
+///
+/// Note, if the secret is the result of a collision, then it's likely that de-serialization will fail(depends
+/// on the format) - if not, then it's extremely likely that hash check will fail. If not, you should buy a lottery ticket
+pub fn decode_secret<S: DeserializeOwned + Hash>(secret: FP) -> Result<Option<S>, anyhow::Error> {
+
+    if secret == 0u32.into() {
+        return Ok(None)
+    }
+
+    let bytes = secret.to_bytes_array();
+
+    let value_and_hash = bincode::deserialize::<ValueAndHash<S>>(&bytes)?;
+    let mut hasher = DefaultHasher::new();
+    value_and_hash.value.hash(&mut hasher);
+    let computed_hash = hasher.finish();
+    if computed_hash != value_and_hash.hash {
+        return Err(anyhow::anyhow!("Decoded value has wrong hash, it was probably the result of a collision"));
+    }
+    Ok(Some(value_and_hash.value))
+}
+
+pub fn add_shares(share1: &Share, share2: &Share) -> Share {
+    assert_eq!(share1.x, share2.x, "Cannot add shares with different X coordinates");
+    return Share {
+        x: share1.x,
+        p_x: share1.p_x + share2.p_x
+    }
 }
 
 #[cfg(test)]
@@ -113,4 +189,82 @@ mod tests {
         }
     }
 
+    #[test]
+    fn can_encode_and_decode_secret() {
+        let secret = encode_secret("shalom").unwrap();
+        let decoded = decode_secret::<String>(secret).unwrap();
+        assert_eq!(decoded, Some("shalom".into()), "decode mismatch");
+    }
+
+
+    #[test]
+    fn decode_flow_no_collision() {
+        let secret_a = encode_secret("shalom").unwrap();
+        let secret_b = encode_secret("bye").unwrap();
+
+        let zero_secret = encode_zero_secret();
+
+        // say A wants to send over channel A (and 0 over rest)
+        let client_a_chan_a = create_share(secret_a, 2, 2);
+        let client_a_chan_b = create_share(zero_secret, 2, 2);
+
+        // and B wants to send over channel B (and 0 over rest)
+
+        let client_b_chan_a = create_share(zero_secret, 2, 2);
+        let client_b_chan_b = create_share(secret_b, 2, 2);
+
+        // note that both server A and B would have an idetnical view of the following share vector by now
+        let server_chan_a_shares = vec![
+            add_shares(&client_a_chan_a[0], &client_b_chan_a[0]),
+            add_shares(&client_a_chan_a[1], &client_b_chan_a[1]),
+        ];
+
+        let server_chan_a_decoded_val = decode_secret::<String>(reconstruct_secret(server_chan_a_shares.as_slice(), 2)).unwrap();
+        assert_eq!(server_chan_a_decoded_val, Some("shalom".into()), "decoded value mismatch");
+
+        
+        let server_chan_b_shares = vec![
+            add_shares(&client_a_chan_b[0], &client_b_chan_b[0]),
+            add_shares(&client_a_chan_b[1], &client_b_chan_b[1]),
+        ];
+
+        let sever_chan_b_decoded_val = decode_secret::<String>(reconstruct_secret(server_chan_b_shares.as_slice(), 2)).unwrap();
+        assert_eq!(sever_chan_b_decoded_val, Some("bye".into()), "decoded value mismatch");
+    }
+
+    #[test]
+    fn decode_flow_with_collision() {
+        let secret_a = encode_secret("shalom").unwrap();
+        let secret_b = encode_secret("bye").unwrap();
+
+        let zero_secret = encode_zero_secret();
+
+        // say A wants to send over channel A (and 0 over rest)
+        let client_a_chan_a = create_share(secret_a, 2, 2);
+        let client_a_chan_b = create_share(zero_secret, 2, 2);
+
+        // and B wants to send over channel A as well
+
+        let client_b_chan_a = create_share(secret_b, 2, 2);
+        let client_b_chan_b = create_share(zero_secret, 2, 2);
+
+
+        // note that both server A and B would have an idetnical view of the following share vector by now
+        let server_chan_a = vec![
+            add_shares(&client_a_chan_a[0], &client_b_chan_a[0]),
+            add_shares(&client_a_chan_a[1], &client_b_chan_a[1]),
+        ];
+
+
+        let _expected_err = decode_secret::<String>(reconstruct_secret(server_chan_a.as_slice(), 2)).unwrap_err();
+
+        
+        let server_chan_b_shares = vec![
+            add_shares(&client_a_chan_b[0], &client_b_chan_b[0]),
+            add_shares(&client_a_chan_b[1], &client_b_chan_b[1]),
+        ];
+
+        let server_chan_b_decoded_val = decode_secret::<String>(reconstruct_secret(server_chan_b_shares.as_slice(), 2)).unwrap();
+        assert_eq!(server_chan_b_decoded_val, None, "channel B should have no value");
+    }
 }
