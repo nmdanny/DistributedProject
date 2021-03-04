082d0b8ec35095e636c41958d550c5cd287d521c
Daniel Kerbel <nmdanny@gmail.com>
2021-03-04 21:26:54 +0200

    add bash script for starting servers

---
+++ start_servers.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+num_servers=5
+threshold=3
+num_clients=3
+num_channels=30
+
+timeoutMs=3000
+
+
+exe="./target/debug/runner"
+
+session="DistLib"
+
+tmux set -g remain-on-exit on
+tmux new-session -d -s $session
+tmux attach-session -t $session
+
+
+WORK_DIR=$(mktemp -d)
+
+for ((id=0; id < $num_servers; id++))
+do
+    tmux new-window -a -n "Server $id" "$exe --num_servers $num_servers -t $threshold --num_clients $num_clients --num_channels $num_channels --timeout $timeoutMs server --id $id &> $WORK_DIR/s-$id &; less -R $WORK_DIR/s-$id"
+done
+
+for ((id=0; id < $num_clients; id++))
+do
+    tmux new-window -a -n "Client $id" "$exe --num_servers $num_servers -t $threshold --num_clients $num_clients --num_channels $num_channels --timeout $timeoutMs client --id $id &> $WORK_DIR/c-$id &; less -R $WORK_DIR/c-$id"
+done
