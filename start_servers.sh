2ddf679104915ed125bd39e3d7c9bd3ebdce12f1
Daniel Kerbel <nmdanny@gmail.com>
2021-03-09 17:01:08 +0200

    update linux start script

--- start_servers.sh
+++ start_servers.sh
@@ -15,7 +15,6 @@ exe="./target/release/runner"
 session="DistLib"
 
 tmux set -g remain-on-exit on
-tmux kill-session -t $session
 tmux new-session -d -s $session
 
 
@@ -23,13 +22,12 @@ WORK_DIR=$(mktemp -d)
 
 for ((id=0; id < $num_servers; id++))
 do
-    tmux new-window -a -n "Server $id" "$exe --num_servers $num_servers -t $threshold --num_clients $num_clients --num_channels $num_channels --timeout $timeoutMs server --id $id &> $WORK_DIR/s-$id &; tail -f $WORK_DIR/s-$id"
+    tmux new-window -a -n "Server $id" "$exe --num_servers $num_servers -t $threshold --num_clients $num_clients --num_channels $num_channels --timeout $timeoutMs --phase_length $phaseLength server --id $id &> $WORK_DIR/s-$id &; tail -f $WORK_DIR/s-$id"
 done
 
 for ((id=0; id < $num_clients; id++))
 do
-    tmux new-window -a -n "Client $id" "$exe --num_servers $num_servers -t $threshold --num_clients $num_clients --num_channels $num_channels --timeout $timeoutMs client --id $id &> $WORK_DIR/c-$id &; tail -f $WORK_DIR/c-$id"
+    tmux new-window -a -n "Client $id" "$exe --num_servers $num_servers -t $threshold --num_clients $num_clients --num_channels $num_channels --timeout $timeoutMs --phase_length $phaseLength client --id $id &> $WORK_DIR/c-$id &; tail -f $WORK_DIR/c-$id"
 done
-
 
 tmux attach-session -t $session
